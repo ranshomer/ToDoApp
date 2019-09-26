875018c4ba6714890e2dcbf4b61e3476e7dc8f6f
ranshomer <ranshomer@gmail.com>
2019-09-26 20:28:16 +0300

    change port

--- src/api/TodoRequestOptions.js
+++ src/api/TodoRequestOptions.js
@@ -4,9 +4,9 @@ class TodoRequestOptions {
 	constructor(host, todoUrl) {
 		this.host = host ? host : 'http://' + 
  			(process.env.TODO_BACKEND ? process.env.TODO_BACKEND : 'localhost') + ':' + 
- 			(process.env.TODO_BACKEND_PORT ? process.env.TODO_BACKEND_PORT : '3001') + '/';
+ 			(process.env.TODO_BACKEND_PORT ? process.env.TODO_BACKEND_PORT : '3000') + '/';
 		this.todoUrl = todoUrl ? todoUrl : 'api/Todos/';
-	}
+	}s
 
 	getUrlHost() {
 		return this.host;
